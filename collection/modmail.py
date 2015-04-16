2b8bb6cd1d0c7508a9d197d3dae5ef927fa2df6b
Delirium Tremens <jake@schwartzpub.com>
2015-04-16 17:50:13 -0500

    Added replies and utc time

--- collection/modmail.py
+++ collection/modmail.py
@@ -1,25 +1,25 @@
-#!/usr/local/bin/python3.4
-
 import praw,pymongo,sys
 from pymongo import MongoClient
 
-client = MongoClient('ip.of.mongo.server',27017)
+client = MongoClient('192.168.0.115',27017)
 db = client.modmail
 collection = db.mail
 
-r = praw.Reddit("useragent")
-r.login("username", "password")
+r = praw.Reddit("DeliriumTremensTest")
+r.login("DeliriumTremens", "7hzV2y^$U^q%")
 
-subreddit = r.get_subreddit("subreddit")
+subreddit = r.get_subreddit("globaloffensive")
 
 modmail = r.get_mod_mail(subreddit,limit=1000)
 count = 0
 after = ''
 
 for mail in modmail:
-        insertmail = collection.insert_one({'_id':mail.id,'user':str(mail.author),'subject':mail.subject,'body':mail.body})
+        insertmail = collection.insert_one({'_id':mail.id,'utc':mail.created_utc,'user':str(mail.author),'subject':mail.subject,'body':mail.body,'replies':[]})
         count+=1
         after = mail.id
+        for reply in mail.replies:
+                collection.update_one({'_id':mail.id}, {'$push' : {'replies': {'_id':reply.id,'utc':reply.created_utc,'user':str(reply.author),'subject':reply.subject,'body':reply.body}}}, up$
         sys.stdout.write(" Modmails found: %d...    \r" % (count))
 
 while True:
@@ -28,6 +28,8 @@ while True:
                 insertmail = collection.insert_one({'_id':mail.id,'user':str(mail.author),'subject':mail.subject,'body':mail.body})
                 count+=1
                 after = mail.id
+                for reply in mail.replies:
+                        collection.update_one({'_id':mail.id}, {'$push': {'replies': {'_id':reply.id,'utc':reply.created_utc,'user':str(reply.author),'subject':reply.subject,'body':reply.body$
                 sys.stdout.write(" Modmails found: %d...    \r" % (count))
         if count % 1000 > 0:
                 sys.stdout.write(" Modmails found: %d...    \r" % (count))
